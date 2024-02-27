9ac377e328c64e49d6dbf1d5c1f3948750d8b167
None <120342992+Siofr@users.noreply.github.com>
2024-02-27 23:27:08 +0000

    Implemented Turret Model and Next Wave Button
    Title

--- Assets/Scripts/Siofra-AI/Wave Spawner/WaveState.cs
+++ Assets/Scripts/Siofra-AI/Wave Spawner/WaveState.cs
@@ -8,11 +8,6 @@ public class WaveState : MonoBehaviour
     [SerializeField] private GameObject downtimeUI;
     public UnityEvent startWave;
 
-    private void Awake()
-    {
-        downtimeUI = GameObject.Find("DowntimeUI");
-    }
-
     public void StartNextWave()
     {
         downtimeUI.SetActive(false);
