638fa84453b8c4085d675be248c003c724f1366b
None <120342992+Siofr@users.noreply.github.com>
2024-03-13 00:59:03 +0000

    Added New Enemy
    Added:
    - Virus
    
    To Fix:
    - Virus Not Replicating

---
+++ Assets/Scripts/Siofra-AI/Enemies/VirusEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject virusPrefab;
+    public int currentSplit;
+    public int maxSplit;
+
+    [SerializeField] private EnemyBehaviour enemyBehaviourScript;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enemyBehaviourScript.health = enemyBehaviourScript.health / currentSplit;
+        maxSplit = enemyBehaviourScript.enemyStats.splitTimes;
+    }
+
+    public void Split()
+    {
+        for (int i = 0; i < currentSplit + 1; i++)
+        {
+            GameObject splitEnemy = Instantiate(virusPrefab);
+            EnemyBehaviour splitEnemyScript = splitEnemy.GetComponent<EnemyBehaviour>();
+
+            VirusEffect splitEnemyEffectScript = splitEnemy.GetComponent<VirusEffect>();
+            splitEnemyEffectScript.currentSplit = currentSplit++;
+
+            if (currentSplit >= maxSplit)
+            {
+                Debug.Log("Broke");
+                break;
+            }
+
+            splitEnemyScript.enemyPath = enemyBehaviourScript.enemyPath;
+            splitEnemyScript.currentWaypoint = enemyBehaviourScript.currentWaypoint;
+            splitEnemyScript.targetPosition = enemyBehaviourScript.targetPosition;
+
+            splitEnemyScript.health = enemyBehaviourScript.health / splitEnemyEffectScript.currentSplit;
+        }
+        Debug.Log("Split");
+        Destroy(gameObject);
+    }
+}
